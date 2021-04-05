383dd04a18ef5075dc7d57bd2820a58ee6a2341f
Yaroslav Dynnikov <yaroslav.dynnikov@gmail.com>
2021-04-05 16:03:01 +0300

    Review roles setup
    - Enable tutorials tab
    - Enable migrations role
    - Make cartridge-extensions permanent

--- easy-function-scm-1.rockspec
+++ easy-function-scm-1.rockspec
@@ -8,10 +8,11 @@ dependencies = {
     'tarantool >= 2.5',
     'lua >= 5.1',
     'checks == 3.1.0-1',
-    'cartridge == 2.5.1',
+    'cartridge == 2.5.1-1',
     'analytics == scm-1', -- from submodule
     'cartridge-extensions == scm-1',
-    'crud == 0.3.0',
+    'crud == 0.3.0-1',
+    'migrations == 0.4.0-1'
 }
 
 build = {
--- init.lua
+++ init.lua
@@ -6,17 +6,31 @@ package.setsearchroot()
 local front = require('frontend-core')
 local cartridge = require('cartridge')
 local analytics = require('analytics')
+local tutorial = require('easy-function.bundle')
+
+require('cartridge.roles.vshard-router').hidden = true
+require('cartridge.roles.vshard-storage').hidden = true
+require('extensions').permanent = true
 
 local ok, err = cartridge.cfg({
     roles = {
         'cartridge.roles.vshard-router',
         'cartridge.roles.vshard-storage',
-        'extensions',
         'cartridge.roles.crud-router',
         'cartridge.roles.crud-storage',
-    }
+        'extensions',
+        'migrator',
+    },
+    vshard_groups = {
+        default = {bucket_count = 1000},
+    },
 })
 assert(ok, tostring(err))
 
 front.add('analytics_static', analytics.static_bundle)
 front.add('ga', analytics.use_bundle({ ga = '71738660-1' }))
+front.add('tutorial', tutorial)
+
+package.loaded.migrator.set_loader(
+    require('migrator.config-loader').new()
+)
